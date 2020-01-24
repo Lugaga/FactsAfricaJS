77bf130af39cbe1bcea46154726abbbb3edd8b26
lugaga <maureezgaga@gmail.com>
2020-01-24 13:12:48 +0300

    add js folder

---
+++ js/script.js
@@ -0,0 +1,120 @@
+$(document).ready(function(){
+    $("#btn1").click(function(){
+      $(".tr1").fadeOut(1000);
+    });
+  })
+
+$(document).ready(function(){
+    $("#btn2").click(function(){
+      $(".tr2").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn3").click(function(){
+      $(".tr3").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn4").click(function(){
+      $(".tr4").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn5").click(function(){
+      $(".tr5").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn6").click(function(){
+      $(".tr6").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn7").click(function(){
+      $(".tr7").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn8").click(function(){
+      $(".tr8").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn9").click(function(){
+      $(".tr9").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn10").click(function(){
+      $(".tr10").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn11").click(function(){
+      $(".tr11").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn12").click(function(){
+      $(".tr12").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn13").click(function(){
+      $(".tr13").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn14").click(function(){
+      $(".tr14").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn15").click(function(){
+      $(".tr15").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn16").click(function(){
+      $(".tr16").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn17").click(function(){
+      $(".tr17").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn18").click(function(){
+      $(".tr18").fadeOut(1000);
+   });
+  })
+
+  $(document).ready(function(){
+    $("#btn19").click(function(){
+      $(".tr19").fadeOut(1000);
+   });
+  })
+
+  $(document).ready(function(){
+    $("#btn20").click(function(){
+      $(".tr20").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn21").click(function(){
+      $(".tr21").fadeOut(1000);
+   });
+  })
+  $(document).ready(function(){
+    $("#btn22").click(function(){
+      $("tr").fadeOut(2000);
+   });
+  })
+
+
+
+
+
+
+
